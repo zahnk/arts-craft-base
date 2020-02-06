c3a6ef99aef0c553cda0fe72a4881d11e09b02fc
Zahn <zahnk@media-saturn.com>
2020-02-06 14:40:20 +0100

    implemented component creation

--- client/src/components/ComponentCreate.js
+++ client/src/components/ComponentCreate.js
@@ -7,25 +7,74 @@ export default class ComponentCreate extends Component {
   constructor(props){
     super(props)
     this.state = {
-      component: []
+      name:         "",
+      description:  "",
+      imageUrl:     "",
+      owner:        this.props.user,
+      template:     ""
+      //projects:     []
     }
   }
 
-  handleSubmit = () => {
-    console.log ("submit/ create new component");
-    //this.createComponent;
+  handleChange = event => {
+    console.log ("entry new component");
+    this.setState({ 
+      [event.target.name]: event.target.value 
+    });
   }
 
-  handleChange = () => {
-    console.log ("entry new component");
-    //this.createComponent;
+  handleCancel = event => {
+    this.props.history.push("/components"); 
   }
+  
+
+
+  handleSubmit = event => {
+     if (event) {
+      event.preventDefault();
+    }
+    console.log("SUBMIT", this.state);
+    // check if the image is already uploaded to the cloud or no image was selected
+    
+      // axios.post('http://localhost:5555/api/projects')
+      axios
+        .post("/api/components/create", {
+          name: this.state.name,
+          owner: this.state.owner,
+          description: this.state.description,
+          imageUrl: this.state.imageUrl,
+          template: this.state.template
+          //projects: this.state.projects 
+        })
+        .then(response => {
+          console.log("then after post");
+          //this.props.refreshData();
+          this.setState({
+            name:         "",
+            description:  "",
+            template:     "",
+            imageUrl: ""
+            //projects: []
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+       // set a flag that the project got submitted
+      this.setState({
+        submitted: true
+      })
+  }
+
+ 
 
   render() {
+    console.log(this.state);
+    
     return (
       <div>
         <Card bg="secondary" text="white" style={{marginBottom: "10px"}}>
-          <Card.Header as="h2"><i className="fas fa-sitemap fa-a"></i>Componentes Create</Card.Header>
+          <Card.Header as="h2"><i className="fas fa-sitemap fa-a"></i>Components Create</Card.Header>
         </Card>
 
         <Card style={{marginBottom: "10px", textAlign: "left" }}>
@@ -37,9 +86,9 @@ export default class ComponentCreate extends Component {
                   <Form.Control
                     as="input"
                     type="text"
-                    name="componentName"
-                    id="componentName"
-                    value={this.state.componentName}
+                    name="name"
+                    id="name"
+                    value={this.state.name}
                     onChange={this.handleChange}
                   />
                 </Form.Group>
@@ -48,37 +97,37 @@ export default class ComponentCreate extends Component {
                   <Form.Control style={{minHeight: "50px"}}
                     rows="5"
                     as="textarea"
-                    name="componentDescription"
-                    id="componentDescription"
-                    value={this.state.componentDescription}
+                    name="description"
+                    id="description"
+                    value={this.state.description}
                     onChange={this.handleChange}
                   />
                 </Form.Group>
               </Form.Row>
               <Form.Row>
                 <Form.Group as={Col} md="4">
-                  <Form.Label htmlFor="componentType">Type:</Form.Label>
+                  <Form.Label htmlFor="imageUrl">Image Url:</Form.Label>
                   <Form.Control
-                    as="select"
-                    name="componentType"
-                    id="componentType"
+                    as="input"
+                    name="imageUrl"
+                    id="imageUrl"
                     onChange={this.handleChange}
                   />
                 </Form.Group>
                 <Form.Group as={Col} md="8">
-                  <Form.Label htmlFor="componentNotes">Notes: </Form.Label>
+                  <Form.Label htmlFor="template">Template: </Form.Label>
                   <Form.Control
                     as="input"
                     type="text"
-                    name="componentNotes"
-                    id="componentNotes"
-                    value={this.state.componentNotes}
+                    name="template"
+                    id="template"
+                    value={this.state.template}
                     onChange={this.handleChange}
                   />
                 </Form.Group>
               </Form.Row>
-            
-              <Button className="mr-2" size="lg" variant="primary" type="submit" onClick={this.handleCreate}><i className="far fa-save fa-lg fa-a"></i>Submit new Component</Button>
+              <Button className="mr-2" size="lg" variant="primary" type="submit" onClick={this.handleCancel}><i className="far fa-window-close fa-lg fa-a"></i>Cancel</Button>            
+              <Button className="mr-2" size="lg" variant="primary" type="submit"><i className="far fa-save fa-lg fa-a"></i>Submit new Component</Button>
             </Form>
           </Card.Body>
         </Card>
