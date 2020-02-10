3af48189a8507151b386d269175c1769b4e7351a
MSHSusanneV <voglsus@mediamarktsaturn.com>
2020-02-10 13:43:29 +0100

    Zwischenstand Component Edit/Save

--- client/src/components/ComponentDetail.js
+++ client/src/components/ComponentDetail.js
@@ -4,14 +4,19 @@ import ConfirmDelete from "./ConfirmDelete";
 import axios from "axios";
 import '@fortawesome/fontawesome-free/css/all.css';
 
+
 class ComponentDetail extends Component {
   state = {
     component: {
       name: '',
       description: '',
       imageUrl: '',
-      template: ''
-    }
+      notes: ''
+    },
+    error: "",
+    editForm: false,
+    showConfirm: false
+
   };
 
   showConfirmDelete = () => {
@@ -33,12 +38,6 @@ class ComponentDetail extends Component {
 
   }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
   deleteComponent = (componentId) => {
     //console.log("im delete.js gelandet", componentId);
     const deletePath = `/api/components/${componentId}`;
@@ -49,6 +48,19 @@ class ComponentDetail extends Component {
       .catch(err => {
         console.log(err);
       })
+  };
+
+  toggleEdit = () => {
+    this.setState({
+      editForm: !this.state.editForm
+    });
+  };  
+    
+  handleChange = event => {
+  console.log ("handleChange", event.target.name)  
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
   getData = () => {
@@ -82,65 +94,133 @@ class ComponentDetail extends Component {
       return <div></div>;
     }
 
-    return (
+  
+
+
+
+
+
+
+
+
+let form;
+if (this.state.editForm) {
+  form = <Card style={{ marginBottom: "10px", textAlign: "left" }}>
+    <Card.Body>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Component Name: </Form.Label>
+            <Form.Control
+              as="input"
+              type="text"
+              name="name"
+              value={this.state.name || ''}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="8">
+            <Form.Label>Description: </Form.Label>
+            <Form.Control style={{ minHeight: "50px" }}
+              rows="5"
+              as="textarea"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Image:</Form.Label>
+            <Form.Control
+                as="input"
+                type="text"
+                name="imageUrl"
+                value={this.state.imageUrl}
+                onChange={this.handleChange}
+            />           
+          </Form.Group>
+          <Form.Group as={Col} md="8">
+            <Form.Label>Notes: </Form.Label>
+            <Form.Control
+              rows="5"
+              as="textarea"
+              name="notes"
+              value={this.state.notes}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+        </Form.Row>
+        <Button className="mr-5" size="lg" variant="primary" onClick={() => { this.props.history.push("/components") }}><i className="far fa-window-close fa-lg fa-a"></i>Cancel</Button>
+        <Button onClick={this.toggleEdit} className="mr-5 ml-5" size="lg"><i className="far fa-save fa-lg fa-a"></i>Save</Button>
+        <Button onClick={this.showConfirmDelete} className="ml-5" size="lg"><i className="far fa-trash-alt fa-lg fa-a"></i>Delete</Button>
+      </Form>
+    </Card.Body>
+  </Card>;
+} else {
+  form = <Card style={{ marginBottom: "10px", textAlign: "left" }}>
+    <Card.Body>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Component Name: </Form.Label>
+            <Form.Control
+              readOnly
+              as="input"
+              type="text"
+              value={this.state.component.name}
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="8">
+            <Form.Label>Description: </Form.Label>
+            <Form.Control style={{ minHeight: "50px" }}
+              readOnly
+              rows="5"
+              as="textarea"
+              value={this.state.component.description}
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Image: </Form.Label>
+            <Form.Control
+              readOnly={true}
+              type="text"
+              value={this.state.component.imageUrl}
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="8">
+            <Form.Label>Notes: </Form.Label>
+            <Form.Control
+              readOnly
+              rows="5"
+              as="textarea"
+              value={this.state.component.notes}
+            />
+          </Form.Group>
+        </Form.Row>
+        <Button className="mr-5" size="lg" variant="primary" onClick={() => { this.props.history.push("/components") }}><i className="far fa-window-close fa-lg fa-a"></i>Cancel</Button>
+        <Button onClick={this.toggleEdit} className="mr-5 ml-5" size="lg"><i className="far fa-edit fa-lg fa-a"></i>Edit</Button>
+        <Button onClick={this.showConfirmDelete} className="ml-5" size="lg"><i className="far fa-trash-alt fa-lg fa-a"></i>Delete</Button>
+      </Form>
+    </Card.Body>
+  </Card>;
+}
+
+return (
       <div>
         <Card bg="secondary" text="white" style={{ marginBottom: "10px" }}>
-          <Card.Header as="h2"><i className="fas fa-sitemap fa-a"></i>Project Detail</Card.Header>
+          <Card.Header as="h2"><i className="fas fa-sitemap fa-a"></i>Component Detail</Card.Header>
         </Card>
 
-        <Card style={{ marginBottom: "10px", textAlign: "left" }}>
-          <Card.Body>
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} md="4">
-                  <Form.Label>Project Name: </Form.Label>
-                  <Form.Control
-                    readOnly
-                    as="input"
-                    type="text"
-                    value={this.state.component.name}
-                  />
-                </Form.Group>
-                <Form.Group as={Col} md="8">
-                  <Form.Label>Description: </Form.Label>
-                  <Form.Control style={{ minHeight: "50px" }}
-                    readOnly
-                    rows="5"
-                    as="textarea"
-                    value={this.state.component.description}
-                  />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md="4">
-                  <Form.Label>Image:</Form.Label>
-                  <Form.Control
-                    readOnly={true}
-                    type="text"
-                    value={this.state.component.imageUrl}
-                  />
-                </Form.Group>
-                <Form.Group as={Col} md="8">
-                  <Form.Label>Notes: </Form.Label>
-                  <Form.Control
-                    readOnly
-                    rows="5"
-                    as="textarea"
-                    value={this.state.component.template}
-                  />
-                </Form.Group>
-              </Form.Row>
-              <Button className="mr-5" size="lg" variant="primary" onClick={() => { this.props.history.push("/components") }}><i className="far fa-window-close fa-lg fa-a"></i>Cancel</Button>
-              <Button className="mr-5 ml-5" size="lg"><i className="far fa-edit fa-lg fa-a"></i>Edit</Button>
-              <Button onClick={this.showConfirmDelete} className="ml-5" size="lg"><i className="far fa-trash-alt fa-lg fa-a"></i>Delete</Button>
-            </Form>
-          </Card.Body>
-        </Card>
+      {form}
+
         <Card style={{ marginBottom: "10px", textAlign: "left" }}>
           <Card.Body>
           </Card.Body>
         </Card>
-
         <ConfirmDelete show={this.state.showConfirm} close={this.deleteComponentConfirmed} title="Component" />
       </div>
     );
